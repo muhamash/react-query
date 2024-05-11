27a1c69761fdb42befbaf5c8ec3ca23b078b8a1e
muhamash <muhammad-ashraful@outlook.com>
2024-05-11 23:37:15 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-green-800 text-center text-3xl text-bold py-10">
+      Vite TailwindCss react query crash
+    </div>
+  )
+}
+
+export default App
